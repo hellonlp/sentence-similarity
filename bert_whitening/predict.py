2c261e2322705bb977664599a0841eb885c160f6
陈明 <319407@qq.com>
2024-01-27 00:05:08 +0800

    Update predict.py

--- bert_whitening/predict.py
+++ bert_whitening/predict.py
@@ -9,6 +9,7 @@ Created on Tue Nov 15 16:44:40 2022
 import heapq
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
+
 from bert_whitening.utils import load_model,load_vectors
 from bert_whitening.hyperparameters import Hyperparamters as hp
 from bert_whitening.modules import get_tokenizer,get_encoder
@@ -25,38 +26,8 @@ vectors, querys = load_vectors(hp.vectors_path)
 tokenizer = get_tokenizer(hp.vocab_path)
 encoder = get_encoder(hp.config_path, hp.checkpoint_path)
 
-
-
-def get_similarity_topN(sentence, topN=10):
-    """
-    Parameters
-    ----------
-    sentence : TYPE string
-        DESCRIPTION. A sentence.
-    topN : TYPE int
-        DESCRIPTION. The default is 5.
-
-    Returns
-    -------
-    outputs : a list of tuple
-        DESCRIPTION. The result tuples like:
-            (0.5286, '一架飞机正在起飞。')
-            (0.5283, '有些人在战斗。')
-            (0.5271, '一架飞机要起飞了。')
-            (0.5265, '一个人在切黄瓜。')
-            (0.5265, '一个女孩在给她的头发做发型。')
-    """
-    vec = convert_sentence_to_vecs(sentence, tokenizer, encoder)
-    trans_vec = transform_and_normalize(vec, kernel, bias)
-    similarity_list = cosine_similarity(trans_vec, vectors).tolist()[0]
-    topk_index = heapq.nlargest(topN, range(len(similarity_list)), similarity_list.__getitem__)
-    outputs = []
-    for i in topk_index:
-        outputs.append((np.round(similarity_list[i],4),querys[i]))
-    return outputs
-
     
-def get_similarity_topN_new(sentence, texts, topN=10):  
+def get_similarity_topn(sentence, texts, topN=10):  
     vec = convert_sentence_to_vecs(sentence, tokenizer, encoder)
     trans_vec = transform_and_normalize(vec, kernel, bias)
     texts_vec = convert_sentences_to_vecs(texts, tokenizer, encoder)
@@ -81,7 +52,7 @@ def get_similarity_two(sentence1, sentence2, topN=10):
 if __name__ == '__main__':    
     # 和历史数据对比
     sentence = '一架飞机正在起飞。'
-    results = get_similarity_topN(sentence)
+    results = get_similarity_topn(sentence)
     for l in results:
         print(l)
     
@@ -89,11 +60,11 @@ if __name__ == '__main__':
     texts = ['你怎么样','我吃了一个苹果','你过的好吗','你还好吗','你',
              '你好不好','你好不好呢','我不开心']
     sentence = '你好吗'
-    results = get_similarity_topN_new(sentence,texts)
+    results = get_similarity_topn(sentence,texts)
     for l in results:
         print(l)   
     
-    #
+    # 2个句子之间
     sentence1 = '你好吗'
     sentence2 = '你过的好吗'
     print(get_similarity_two(sentence1,sentence2))
